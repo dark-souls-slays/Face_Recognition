47bd4dd20e714234ae003fcaa25e04ec476aa4ca
dark-souls-slays <n3m0a3u@gmail.com>
2018-07-03 01:22:05 +0800

    dataset 1D and Y labels / images in greyscale

--- FR.py
+++ FR.py
@@ -1,41 +1,46 @@
-"""
-# Python
-import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.Session()
-print(sess.run(hello))
-"""
 import numpy as np
 from PIL import Image
 import glob
 import os, os.path
 import tensorflow as tf
 import matplotlib.pyplot as plt
-
+from sklearn.datasets import make_classification
 #Load 650 images as (261, 161, 3) -> (261,161) dimension
 def inputs():
     imgs = []
     dataset = []
+    y = []
+    check = 0
+    size = 128, 128
     path = "/Users/ClaudiaEspinoza/Desktop/Face Recognition System/Face Database"
     for f in os.listdir(path):
         im = Image.open(os.path.join(path,f))
-        im = im.convert('1') # convert image to black and white
-        #im.save("r.jpg")
-        im.load()
+        im = im.convert('L')
+        im = im.resize((size), Image.ANTIALIAS)
+        #im.load()
         data = np.array(im)
-        data = data.astype(int)
         im.close()
+        data = np.reshape(data,16384)
         dataset.append(data)
+        y.append(int(f[1:3]))
+        print("image" + f[1:3])
+        print(data)
+
     dataset = np.array(dataset)
-    print(dataset[0])
-    return dataset
-
-
+    #CHECK THAT THIS PARTICULAR  IMAGE CORRESPONDS WITH ITS Y LABEL
+    m = Image.fromarray(dataset[649].reshape(128,128), 'L')
+    m.save("this.jpg")
+    print(y[649])
+    return (dataset,y)
+"""
+def classifier():
+    samples = 1000
+    features = 3
+    x, y = make_classification(n_samples=samples, n_features=features, n_redundant=0,
+	n_classes=50, n_clusters_per_class=3, n_informative=3)
+    print(x.shape)
+    print(y.shape)
+"""
 dataset = inputs()
-"""
-from scipy.misc import face
-img = face(gray=True)
-print(img.shape)
-plt.imshow(img, cmap=plt.cm.gist_gray)
-plt.show()
-"""
+#classifier()
+#softmax used to get the probabilities of each class
